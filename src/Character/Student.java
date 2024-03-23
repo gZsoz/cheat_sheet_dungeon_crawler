7c74123a46e0976c24023838d935cb03e4fd79c8
Személyi Balázs András <szemelyi.balazs@gmail.com>
2024-03-23 19:25:19 +0100

    Character, Student, Teacher

--- src/Character/Student.java
+++ src/Character/Student.java
@@ -1,21 +1,41 @@
 package Character;
 
 import Items.Item;
+import SkeletonUtil.SkeletonUtil;
 
 /**
- * Class representing a student character in the game.
+ * A Student osztály felelős a hallgatók inventory-jában lévő aktiválható tárgyak aktiválásáért.
  */
 public class Student extends Character {
+	
+	
+	/**
+	 * Konstruktor egy hallgató létrehozásához. Student-re állítja a nevet.
+	 */
+	public Student() {
+		name = "Student";
+	}
+	
+	/**
+	 * Konstruktor egy hallgató létrehozásához.
+	 * @param name A hallgató neve.
+	 */
+	public Student(String name) {
+		this.name = name;
+	}
+	
     /**
-     * Activates the specified item.
-     * @param i The item to activate.
+     * A paraméterül kapott tárgy hatásának megkezdése.
+     * @param i A tárgy.
      */
     public void activate(Item i) {
-        // Implementation
+    	SkeletonUtil.printLog(name + ".activate(Item)");
+		SkeletonUtil.increaseIndent();
+		i.use();
+		SkeletonUtil.decreaseIndent();
     }
 
     @Override
     public void update() {
-        // Implementation
     }
 }
